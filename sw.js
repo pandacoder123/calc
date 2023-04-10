1629e411b5692ae4f947045da0bb46516299076a
panda <91168831+pandacoder123@users.noreply.github.com>
2023-04-09 17:08:37 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,28 @@
-self.addEventListener('install', function(event) {
+const cacheName = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/style.css',
+  '/icon/fox.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/sw.js'
-      ]);
-    })
+    caches.open(cacheName)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      })
   );
 });
